f753f82b6bbf66df9ed6bfcee249037dca7cb405
Fireman9143 <168347053+Fireman9143@users.noreply.github.com>
2024-07-24 22:26:24 -0500

    Add files via upload
    Added morse sound FX

--- NewPlan.py
+++ NewPlan.py
@@ -1,6 +1,6 @@
 import sys
+from os import path
 import pygame
-from settings import Settings
 from button import Button
 from morse import morse_code
 
@@ -11,20 +11,22 @@ class MainGame:
     def __init__(self):
         """Initialize the game"""
         pygame.init()
+        pygame.mixer.init()
         self.clock = pygame.time.Clock()
-        self.settings = Settings()
-
+        self.bg_color = (0, 0, 0)
+        
         #Create game window  
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen_width = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Learn Morse Code")
         
         self.game_active = False
         self.play_button = Button(self, "Play")
         self.user_text = ""
         self.ditdah = ""
-
+        self.text_font = pygame.font.SysFont("notomono", self.screen_height//20)
+        self.morse_font = pygame.font.SysFont("freeserif", self.screen_height//40)
         
     def _check_events(self):
         """Check for key presses to start, enter text, or exit"""
@@ -39,9 +41,12 @@ class MainGame:
                     if self.game_active == False:
                         self.game_active = True
                 elif event.key == pygame.K_BACKSPACE:
-                    self.user_text = self.user_text[:-1]
+                    self.user_text = self.user_text[:-2]
                 elif event.key != pygame.K_SPACE:
-                    self.user_text += event.unicode + ""
+                    self.user_text += event.unicode + " "
+                    letter = event.unicode.upper()
+                    if letter in morse_code and len(morse_code[letter]) > 1:
+                            pygame.mixer.Sound(morse_code[letter][1]).play()
                 elif event.key == pygame.K_SPACE:
                     self.user_text += " "
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -60,7 +65,7 @@ class MainGame:
 
     def _update_screen(self):
         """Update the screen"""
-        self.screen.fill(self.settings.bg_color)
+        self.screen.fill(self.bg_color)
         if not self.game_active:
             self.play_button.draw_button()
         if self.game_active:
@@ -72,15 +77,15 @@ class MainGame:
         """Function to wrap lines"""
         user_input = self.user_text.split(" ")
         converted = self.ditdah.split(" ")
-        font_x, font_y = self.settings.text_font.size(" ")
-        font_x = font_x + 40
-        morse_x, morse_y = self.settings.morse_font.size(" ")
-        morse_x = morse_x + 8
+        font_x, font_y = self.text_font.size(" ")
+        font_x = font_x + 15
+        morse_x, morse_y = self.morse_font.size(" ")
+        morse_x = morse_x + 6
 
         x,y = pos
         
         for words in user_input:
-            word_surface = self.settings.text_font.render(words, True, 'green')
+            word_surface = self.text_font.render(words, True, 'green')
             word_width, word_height = word_surface.get_size()
             if x + word_width >= self.screen.get_width():
                 #If font reaches edge, reset x to the font starting point pos[0] and change y by font height
@@ -93,7 +98,7 @@ class MainGame:
             a,b = pos
             b = word_height + 100
             for dahs in converted:
-                code_surface = self.settings.morse_font.render(dahs, True, 'lightblue')
+                code_surface = self.morse_font.render(dahs, True, 'lightblue')
                 code_width, _ = code_surface.get_size()
                 if a + code_width >= self.screen.get_width():
                     #If font reaches edge, reset x to the font starting point pos[0] and change y by font height
@@ -111,12 +116,12 @@ class MainGame:
         for num, character in enumerate(self.user_text.upper()):
             #Check to see if the text is already displayed, if not then update lists
             if character in morse_code and num not in character_pos:
-                morse_letters.append(morse_code[character])
-                morse_letters.append("  ")
+                morse_letters.append(morse_code[character][0])
+                morse_letters.append(" ")
                 character_pos.append(num)
             #Handle spaces that are not in dictionary and add spacing for readability
             elif character == " " and morse_letters[:-2] != " ":
-                morse_letters.append("   ")
+                morse_letters.append("  ")
                 character_pos.append(num)
             #Skip anything that's not morse code or a space
             else:
